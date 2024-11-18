881660b99ab21d4397ecf9b419b9112db8477ba2
VishalTuraga <vishalturaga@gmail.com>
2024-11-18 11:55:16 +0530

    Completed the bcdtobin and bintobcd functions

--- BSP/ds1307.c
+++ BSP/ds1307.c
@@ -10,5 +10,56 @@
 /*
  * Covert Decimal to BCD and vice versa
  */
-static uint8_t BCDtoDec(uint8_t BCD);
-static uint8_t DectoBCD(uint8_t Dec);
+static uint8_t BCDtobin(uint8_t BCD);
+static uint8_t bintoBCD(uint8_t bin);
+
+/*********************************************************************
+ * @fn      		  - BCDtobin
+ *
+ * @brief             - This function converts BCD value to binary
+ *
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ *********************************************************************/
+static uint8_t BCDtobin(uint8_t BCD)
+{
+	uint8_t bin;
+	uint8_t A0,A1;
+
+	A1 = (uint8_t)((bin >> 4)*10);
+	A0 = (bin & (uint8_t)0x0F);
+	return (A1+A0);
+}
+
+/*********************************************************************
+ * @fn      		  - bintoBCD
+ *
+ * @brief             - This function converts Binary to BCD
+ *
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ *********************************************************************/
+static uint8_t bintoBCD(uint8_t bin)
+{
+	uint8_t bcd;
+	uint8_t A0,A1;
+
+	bcd = bin;
+	if(bin > 9)
+	{
+		A1 = bin/10; // gives the digit in tens place
+		A0 = bin%10; // gives the digit in ones place
+
+		bcd = (A1 << 4) | A0; //shift the tens digit by 4 digits and add the ones digit if the first 4 bits
+	}
+
+	return bcd;
+}
